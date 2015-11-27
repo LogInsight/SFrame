6e0aea174f442d96d170f456843510bfc8f3d9cd
wangjia <809946525@qq.com>
2015-11-27 03:47:28 -0800

    add sframe test

---
+++ oss_src/sframe/sframe_test.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file: sframe_test.cpp
+ * @author: wangjia
+ * @mail: 89946525@qq.com
+ * @date: 2015/11/27 02:12:22
+ **/
+
+#include "sframe/sframe.hpp"
+
+#include <iostream>
+
+int main() {
+    std::cout << "hello world" << std::endl;
+
+    graphlab::sframe sf;
+
+    return 0;
+}
